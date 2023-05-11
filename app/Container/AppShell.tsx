f4275cacec5f396c515b5dcb333a3c33a6f867cb
Joe Contreras <joe.contreras888@gmail.com>
2023-05-10 21:56:27 -0500

    navbar initial config

---
+++ app/Container/AppShell.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+
+export default function AppShellDemo() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const [burgerOpen, setBurgerOpen] = useState(false);
+
+  return (
+    <AppShell
+      hidden={burgerOpen}
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <button onClick={() => setBurgerOpen((o) => !o)}>close</button>
+          </div>
+        </Header>
+      }
+    >
+      {burgerOpen && (
+        <Burger
+          opened={!burgerOpen}
+          onClick={() => setBurgerOpen((o) => !o)}
+          size="sm"
+          color={theme.colors.gray[6]}
+          mr="xl"
+        />
+      )}
+    </AppShell>
+  );
+}
---
+++ app/Navbar/Navbar.tsx
@@ -0,0 +1,96 @@
+import {
+  Navbar,
+  Group,
+  Code,
+  ScrollArea,
+  createStyles,
+  rem,
+} from "@mantine/core";
+import {
+  IconNotes,
+  IconCalendarStats,
+  IconGauge,
+  IconPresentationAnalytics,
+  IconFileAnalytics,
+  IconAdjustments,
+  IconLock,
+} from "@tabler/icons";
+import { LinksGroup } from "~/NavbarLinksGroup/NavbarLinksGroup";
+
+const mockdata = [
+  { label: "Dashboard", icon: IconGauge },
+  {
+    label: "Market news",
+    icon: IconNotes,
+    initiallyOpened: true,
+    links: [
+      { label: "Overview", link: "/" },
+      { label: "Forecasts", link: "/" },
+      { label: "Outlook", link: "/" },
+      { label: "Real time", link: "/" },
+    ],
+  },
+  {
+    label: "Releases",
+    icon: IconCalendarStats,
+    links: [
+      { label: "Upcoming releases", link: "/" },
+      { label: "Previous releases", link: "/" },
+      { label: "Releases schedule", link: "/" },
+    ],
+  },
+  { label: "Analytics", icon: IconPresentationAnalytics },
+  { label: "Contracts", icon: IconFileAnalytics },
+  { label: "Settings", icon: IconAdjustments },
+  {
+    label: "Security",
+    icon: IconLock,
+    links: [
+      { label: "Enable 2FA", link: "/" },
+      { label: "Change password", link: "/" },
+      { label: "Recovery codes", link: "/" },
+    ],
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  links: {
+    marginLeft: `calc(${theme.spacing.md} * -1)`,
+    marginRight: `calc(${theme.spacing.md} * -1)`,
+  },
+
+  linksInner: {
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+  },
+}));
+
+export function NavbarNested() {
+  const { classes } = useStyles();
+  const links = mockdata.map((item, index) => (
+    <LinksGroup {...item} key={item.label} />
+  ));
+
+  return (
+    <Navbar height={800} width={{ sm: 300 }} p="md" className={classes.navbar}>
+      {/*
+      <Navbar.Section className={classes.header}>
+        <Group position="apart">
+          <Logo width={rem(120)} />
+          <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
+        </Group>
+      </Navbar.Section>
+*/}
+
+      <Navbar.Section grow className={classes.links} component={ScrollArea}>
+        <div className={classes.linksInner}>{links}</div>
+      </Navbar.Section>
+    </Navbar>
+  );
+}
